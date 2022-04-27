d1c2f2e94368a58131bd5ce3aa002d31c1f341a9
lyy_yyan <lyy_yyan@163.com>
2022-04-27 18:28:08 +0800

    Demo for communication between threads.

---
+++ src/com/lyy/multithread/DemoThreadLocal.java
@@ -0,0 +1,48 @@
+package com.lyy.multithread;
+
+public class DemoThreadLocal {
+    static class ThreadA implements Runnable {
+        private ThreadLocal<String> threadLocal;
+
+        public ThreadA(ThreadLocal<String> threadLocal) {
+            this.threadLocal = threadLocal;
+        }
+
+        @Override
+        public void run() {
+            threadLocal.set("A");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("ThreadA输出: " + threadLocal.get());
+        }
+    }
+
+    static class ThreadB implements Runnable {
+        private ThreadLocal<String> threadLocal;
+
+        public ThreadB(ThreadLocal<String> threadLocal) {
+            this.threadLocal = threadLocal;
+        }
+
+        @Override
+        public void run() {
+            threadLocal.set("B");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("ThreadB输出: " + threadLocal.get());
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+        // 下面两个线程虽然使用同一个ThreadLocal实例，但是他们各自存取自己当前线程的值
+        new Thread(new ThreadA(threadLocal)).start();
+        new Thread(new ThreadB(threadLocal)).start();
+    }
+}
