1f763fcc5231c8799822e61c019fbe5520a53057
lyy_yyan <lyy_yyan@163.com>
2022-10-21 23:38:26 +0800

    offer P019.

--- src/com/lyy/exercise/offer/P019/Solution.java
+++ src/com/lyy/exercise/offer/P019/Solution.java
@@ -11,28 +11,39 @@ public class Solution {
         Assert.assertEquals(true, isMatch("ab", ".*") );
         Assert.assertEquals(true, isMatch("aab", "c*a*b") );
         Assert.assertEquals(false, isMatch("aa", "a") );
+        Assert.assertEquals(true, isMatch("aaaab", "aa*aab") );
     }
 
-    public boolean ansFlag = false;
     public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
 
-        return true;
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
     }
 
-    public void charMatch(String s, String p, int sStep, int pStep) {
-        if (sStep == s.length()) {
-            if (pStep == p.length()) {
-                ansFlag = true;
-                return;
-            }
-            return;
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
         }
-        if (pStep == p.length()) {
-            if (sStep == s.length()) {
-                ansFlag = true;
-                return;
-            }
-            return;
+        if (p.charAt(j - 1) == '.') {
+            return true;
         }
+        return s.charAt(i - 1) == p.charAt(j - 1);
     }
 }
