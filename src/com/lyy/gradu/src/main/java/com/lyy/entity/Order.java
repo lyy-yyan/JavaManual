544388b5eb86686fa13e1f30d0e3823c94cd985d
lyy_yyan <lyy_yyan@163.com>
2023-05-23 17:59:59 +0800

    简单构建.

---
+++ src/com/lyy/gradu/src/main/java/com/lyy/entity/Order.java
@@ -0,0 +1,37 @@
+package com.lyy.entity;
+
+/**
+ * @author Liu Yuyan
+ */
+public class Order {
+
+    /**
+     * 订单原始价格，即优惠前价格
+     */
+    private Double originalPrice;
+
+    /**
+     * 订单真实价格，即优惠后价格
+     */
+    private Double realPrice;
+
+    public String toString() {
+        return "Order{" + "originalPrice=" + originalPrice + ", realPrice=" + realPrice + '}';
+    }
+
+    public Double getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(Double originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public Double getRealPrice() {
+        return realPrice;
+    }
+
+    public void setRealPrice(Double realPrice) {
+        this.realPrice = realPrice;
+    }
+}
